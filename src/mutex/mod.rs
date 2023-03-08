788d9c249a2c69acdcbdb0a15fc83b0c71df30e4
Martin Kröning <mkroening@posteo.net>
2023-03-08 14:23:04 +0100

    Add StillMutex

--- src/mutex/mod.rs
+++ src/mutex/mod.rs
@@ -1,9 +1,11 @@
 pub(crate) mod interrupt;
 pub(crate) mod spin;
+pub(crate) mod still;
 pub(crate) mod ticket;
 
 use interrupt::RawInterruptMutex;
 use spin::RawSpinMutex;
+use still::RawStillMutex;
 use ticket::RawTicketMutex;
 
 /// An interrupt-safe [`RawSpinMutex`].
@@ -14,6 +16,15 @@ pub type InterruptSpinMutex<T> = lock_api::Mutex<RawInterruptSpinMutex, T>;
 
 /// A [`lock_api::MutexGuard`] based on [`RawInterruptSpinMutex`].
 pub type InterruptSpinMutexGuard<'a, T> = lock_api::MutexGuard<'a, RawInterruptSpinMutex, T>;
+
+/// An interrupt-safe [`RawStillMutex`].
+pub type RawInterruptStillMutex = RawInterruptMutex<RawStillMutex>;
+
+/// A [`lock_api::Mutex`] based on [`RawInterruptStillMutex`].
+pub type InterruptStillMutex<T> = lock_api::Mutex<RawInterruptStillMutex, T>;
+
+/// A [`lock_api::MutexGuard`] based on [`RawInterruptStillMutex`].
+pub type InterruptStillMutexGuard<'a, T> = lock_api::MutexGuard<'a, RawInterruptStillMutex, T>;
 
 /// An interrupt-safe [`RawTicketMutex`].
 pub type RawInterruptTicketMutex = RawInterruptMutex<RawTicketMutex>;
